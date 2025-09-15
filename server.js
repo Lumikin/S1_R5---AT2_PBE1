e4bdea0e8399eea4ca559e7e96ed8d2a3e17fcc2
Lumikin <lumikomio04@gmail.com>
2025-09-15 14:19:16 -0300

    OIOI

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let PORT = 8081;
 
 app.use(express.json());
 
-
+// Rota GET
 app.get('/soma', (req, res) => {
     try {
         res.status(200).json({ message: 'Vamos somar :D' });
@@ -14,13 +14,13 @@ app.get('/soma', (req, res) => {
     }
 });
 
-
+// Rota POST
 app.post('/soma', (req, res) => {
     try {
-        const numeros = req.body.numero;       
+        const { numeros } = req.body;       
         
-        if ((numeros)) {
-            return res.status(400).json({ message: "O campo 'numeros' deve ser um array" });
+        if (!Array.isArray(numeros)) {
+            return res.status(400).json({ message: "Os numeros devem ser preenchidos corretamente!" });
         }
 
         const total = numeros.reduce((acc, curr) => acc + Number(curr), 0);
