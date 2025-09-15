3e0aad56707b4fecfa77201976206f6174cace39
Lumikin <lumikomio04@gmail.com>
2025-09-15 12:37:13 -0300

    inicializando atv2

--- server.js
+++ server.js
@@ -7,29 +7,18 @@ let PORT = 8081
 
 app.use(express.json());
 
-app.get('/usuarios/novo', (req, res) => {
+app.get('/alunos/notas', (req, res) => {
     try {
-        res.status(201).json({ message: 'Oi! faça o login' });
+        res.status(201).json({ message: 'servidor inicializado' });
     } catch (error) {
         res.status(500).json(error);
     }
 })
 
-app.post('/usuarios/novo', (req, res) => {
+app.post('/alunos/notas', (req, res) => {
     try {
-        const { nome, email, senha } = req.body;
-        if (nome.length < 3 || senha.length < 4 || !email.includes("@")){
-            res.status(401).json({message: `Resposta incorreta`})
-        }
-        else{
-            res.status(200).json({message: "usuario criado"})
-        }
+
         
-        let arrayUsers = []
-
-        arrayUsers.push ({nome, email, senha })
-
-        fs.writeFileSync('usuarios.json', JSON.stringify(arrayUsers, null, 2));
 
     } catch (error) {
         res.status(500).json(error);
