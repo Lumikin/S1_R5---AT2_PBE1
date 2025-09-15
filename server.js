192a1f1183dd959a9fbe8cdbff0e35d9fed41cc3
Lumikin <lumikomio04@gmail.com>
2025-09-15 12:40:14 -0300

    OIOIOI

--- server.js
+++ server.js
@@ -7,18 +7,29 @@ let PORT = 8081
 
 app.use(express.json());
 
-app.get('/alunos/notas', (req, res) => {
+app.get('/usuarios/novo', (req, res) => {
     try {
-        res.status(201).json({ message: 'servidor inicializado' });
+        res.status(201).json({ message: 'Oi! faça o login' });
     } catch (error) {
         res.status(500).json(error);
     }
 })
 
-app.post('/alunos/notas', (req, res) => {
+app.post('/usuarios/novo', (req, res) => {
     try {
-
+        const { nome, email, senha } = req.body;
+        if (nome.length < 3 || senha.length < 4 || !email.includes("@")){
+            res.status(401).json({message: `Resposta incorreta`})
+        }
+        else{
+            res.status(200).json({message: "usuario criado"})
+        }
         
+        let arrayUsers = []
+
+        arrayUsers.push ({nome, email, senha })
+
+        fs.writeFileSync('usuarios.json', JSON.stringify(arrayUsers, null, 2));
 
     } catch (error) {
         res.status(500).json(error);
